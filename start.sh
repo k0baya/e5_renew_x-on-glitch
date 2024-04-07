9ee30296b33208e479a8671c3b55f6851f185e22
k0baya <132335755+k0baya@users.noreply.github.com>
2024-04-07 09:29:46 +0800

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,12 @@
+generate_autodel() {
+  cat > auto_del.sh <<EOF
+while true; do
+  rm -rf /app/.git
+  sleep 5
+done
+EOF
+}
+
+[ -e auto_del.sh ] && bash auto_del.sh &
+
+chmod 777 dotnet && exec ./dotnet Microsoft365_E5_Renew_X.dll
